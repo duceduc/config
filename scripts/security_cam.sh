def84e849947f9393cc8cfa016095e92262539b0
duceduc <info@ducsu.com>
2022-02-11 07:38:36 +0900

    Commit on 2022-02-11 07:38

--- scripts/security_cam.sh
+++ scripts/security_cam.sh
@@ -15,7 +15,7 @@ video=/config/www/img/security/cam_record;
 img=/config/www/img/security;
 id=$(date +"%y-%m-%d_%H-%M-%S")security_camera;
 
-http_url=http://192.168.1.20:8123/api/camera_proxy_stream/camera.security?token=$1;
+http_url=http://192.168.1.20:8123/api/camera_proxy_stream/camera.security_genkan?token=$1;
 
 ffmpeg -i $http_url -t 10 -vcodec copy $video/$id.mp4;
 
