97862020df738f7e52b65ec43083c58e63f551eb
duceduc <duceduc@gmail.com>
2026-01-20 18:00:00 +0900

    Commit on 2026-01-20 18:00

--- custom_components/simple_inventory/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="simple_inventory",
-    version="0.4.0",
-    packages=find_packages(),
-    include_package_data=True,
-    package_data={"custom_components.simple_inventory": ["translations/*.json"]},
-)
