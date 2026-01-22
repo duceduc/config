7f02d6d686bae8d4e48d0be6e60b8de0a35eec1e
duceduc <duceduc@gmail.com>
2026-01-22 18:00:00 +0900

    Commit on 2026-01-22 18:00

--- custom_components/parcelsapp/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the Parcels App integration."""
-
-DOMAIN = "parcelsapp"
-CONF_API_KEY = "api_key"
-CONF_DESTINATION_COUNTRY = "destination_country"
-DEFAULT_SCAN_INTERVAL = 900
-SERVICE_TRACK_PACKAGE = "track_package"
-SERVICE_REMOVE_PACKAGE = "remove_package"
