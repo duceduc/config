d49c343b8028e3dfa199c4f828d0e6806497c5ba
duceduc <duceduc@gmail.com>
2026-06-30 18:00:00 +0900

    Commit on 2026-06-30 18:00

--- custom_components/alexa_media/config_flow.py
+++ custom_components/alexa_media/config_flow.py
@@ -43,7 +43,6 @@ from homeassistant.const import (
 from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult, UnknownFlow
 from homeassistant.exceptions import Unauthorized
-from homeassistant.helpers.httpx_client import create_async_httpx_client
 from homeassistant.helpers.network import NoURLAvailableError, get_url
 from homeassistant.util import slugify
 import httpx
@@ -382,10 +381,7 @@ class AlexaMediaFlowHandler(config_entries.ConfigFlow):
                     self.login,
                     str(URL(self.config.get(CONF_HASS_URL)).with_path(AUTH_PROXY_PATH)),
                 )
-                self.proxy.session_factory = lambda: create_async_httpx_client(
-                    self.hass,
-                    verify_ssl=True,
-                    follow_redirects=False,
+                self.proxy.session_factory = lambda: httpx.AsyncClient(
                     timeout=httpx.Timeout(
                         connect=30.0,
                         read=120.0,
