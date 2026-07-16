a440909b6002e941dfbf4722d4f03b60d68b8dca
duceduc <duceduc@gmail.com>
2026-07-16 18:00:00 +0900

    Commit on 2026-07-16 18:00

--- custom_components/simple_inventory/coordinator/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Coordinator package for Simple Inventory."""
-
-from ._analytics import _compute_avg_restock_days
-from ._core import SimpleInventoryCoordinator
-
-__all__ = ["SimpleInventoryCoordinator", "_compute_avg_restock_days"]
